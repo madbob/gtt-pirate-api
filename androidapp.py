807abadeb0461ba08c124230c31e5e1c150ac1fc
Andrea Canale <vivanco11.ac@gmail.com>
2021-11-07 00:05:15 +0100

    redesign

--- androidapp.py
+++ androidapp.py
@@ -9,20 +9,36 @@ from kivy.utils import get_color_from_hex
 from kivymd.uix.card import MDCard
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
+from kivymd.uix.button import MDFlatButton
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.textfield import MDTextField
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+class content(Screen):
+    pass
 kv=            '''
 #:import get_color_from_hex kivy.utils.get_color_from_hex
-
-
+<content>
+    name: "content"
+    id: content
+    orientation: "vertical"
+    spacing: "12dp"
+    size_hint_y: None
+    height: "50dp"
+    GridLayout:
+        name: "content"
+        id: content
+        MDTextField:
+            id:name
+            hint_text: "Numero della fermata"
+    
 
 MDScreen:
-
     MDBottomNavigation:
         panel_color: get_color_from_hex("#eeeaea")
         selected_color_background: get_color_from_hex("#97ecf8")
         text_color_active: 0, 0, 0, 1
-
         MDBottomNavigationItem:
-            name: 'screen 1'
+            name: 'content'
             text: 'Orari'
             icon: 'train'
             badge_icon: "numeric-10"
@@ -31,66 +47,79 @@ MDScreen:
                 id: entries_box
                 cols: 1
                 rows: 20
-            
-                Button:
-                    text: "Create Widgets"
-                    on_release: app.create_widgets()
-
-                Label:
-                    text: "Create Widgetstestedrrrrr"
-                    color: 0,0,0,0
+                    
+            MDFloatingActionButton:
+                icon: "magnify"
+                type: "type_button"
+                on_release: app.show_alert_dialog()
+                pos_hint:{"center_x":.9,"y":0.07}
 
         MDBottomNavigationItem:
             name: 'screen 2'
             text: 'Notizie'
             icon: 'newspaper'
             badge_icon: "numeric-5"
-
             MDLabel:
                 text: 'Discord'
                 halign: 'center'
-
         MDBottomNavigationItem:
             name: 'screen 3'
             text: 'Impostazioni'
             icon: 'cog-outline'
-
             MDLabel:
                 text: 'LinkedIN'
                 halign: 'center'
 '''
-print("Prova delle API della GTT")
+class Content(Screen):
+    pass
 
-print("Inserisci il numero della fermata:")
-fermata = "2604"
-
-response = requests.get('http://gpa.madbob.org/query.php?stop=' + fermata)
-json_data = response.json()
-
-
-for n in range(len(json_data)):
-        print('Name: ', json_data[n]['line'])
-        print('Orario: ', json_data[n]['hour'])
-        print('Previsioni in tempo reale: ', json_data[n]['realtime'])
 
 class Test(MDApp):
+    dialog = None
     def __init__(self, **kwargs):
         """Construct main app."""
         super().__init__(**kwargs)
         self.name_to_phone = {"Bob Brown": "0414144411", "Cat Cyan": "0441411211", "Oren Ochre": "0432123456"}
+
+    
+    def show_alert_dialog(self):
+        if not self.dialog:
+            self.dialog = MDDialog(
+                title="Inserire il numero della fermata",
+                type="custom",
+                content_cls=Content(),
+                buttons=[
+                    MDFlatButton(
+                        text="OK",
+                        theme_text_color="Custom",
+                        text_color=self.theme_cls.primary_color,
+                        on_release=lambda x:self.create_widgets(),
+                        on_press=lambda x:self.dialog.dismiss()
+                    ),
+                ],
+            )
+        self.dialog.open()
+        
+
+
     def create_widgets(self):
-        """Create buttons from dictionary entries and add them to the GUI."""
+        print("Prova delle API della GTT")
+
+        fermata =  self.root.ids.entries_box.ids.name.text
+
+
+        response = requests.get('http://gpa.madbob.org/query.php?stop=' + fermata)
+        json_data = response.json()
+
         for n in range(len(json_data)):
-            # create a button for each data entry, specifying the text and id
-            # (although text and id are the same in this case, you should see how this works)
             temp_button = Button(text="Linea " + json_data[n]['line'] + "                                           Ora: " + json_data[n]['hour'],
                                  halign="left",
-                                 color=(98, 0, 238, 1),
-                                 background_color=get_color_from_hex("#FFD8E4"),
-                                 background_normal= 'normal.png',
+                                 background_color=get_color_from_hex("#0000FF"),
                                  border=(0,20,0,20))
-            # add the button to the "entries_box" layout widget
             self.root.ids.entries_box.add_widget(temp_button)
+
+
+
     def build(self):
         self.theme_cls.material_style = "M3"
         self.root = Builder.load_string(kv)
