02e14e03e54fe5eb5e9ce750ab6846ddf305fe80
Andrea Canale <vivanco11.ac@gmail.com>
2021-11-08 23:24:00 +0100

    add new features and fix small bug
    add navbar, fix linea inesistente, new button design

--- androidapp.py
+++ androidapp.py
@@ -13,12 +13,30 @@ from kivymd.uix.button import MDFlatButton
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.textfield import MDTextField
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+from kivymd.uix.behaviors import RoundedRectangularElevationBehavior
+from kivymd.uix.card import MDCard
+import sys
 class content(Screen):
     pass
 kv=            '''
 #:import get_color_from_hex kivy.utils.get_color_from_hex
 #: import ScreenManager kivy.uix.screenmanager.ScreenManager
 #: import Screen kivy.uix.screenmanager.ScreenManager
+
+<MD3Card>
+    padding: 16
+
+
+    MDRelativeLayout:
+        size_hint: None, None
+        size: root.size
+
+
+        MDLabel:
+            id: label
+            text: root.text
+            adaptive_size: True
+            color: .2, .2, .2, .8
 
 <content>
     orientation: "vertical"
@@ -31,48 +49,65 @@ kv=            '''
         hint_text: "Numero della fermata"
     
 
-MDScreen:
-    MDBottomNavigation:
-        panel_color: get_color_from_hex("#eeeaea")
-        selected_color_background: get_color_from_hex("#97ecf8")
-        text_color_active: 0, 0, 0, 1
-        MDBottomNavigationItem:
-            name: 'content'
-            text: 'Orari'
-            icon: 'train'
-            badge_icon: "numeric-10"
-            GridLayout:
-                # this layout is populated with Button widgets in app code
-                id: entries_box
-                cols: 1
-                rows: 20
+ScreenManager:
+    Screen:
+        name: "menu"
+        MDBottomNavigation:
+            panel_color: get_color_from_hex("#eeeaea")
+            selected_color_background: get_color_from_hex("#97ecf8")
+            text_color_active: 0, 0, 0, 1
+            MDBottomNavigationItem:
+                name: 'content'
+                text: 'Orari'
+                icon: 'train'
+                badge_icon: "numeric-10"
+                GridLayout:
+                    # this layout is populated with Button widgets in app code
+                    id: entries_box
+                    cols: 1
 
-                    
-            MDFloatingActionButton:
-                icon: "magnify"
-                type: "type_button"
-                on_release: app.show_alert_dialog()
-                pos_hint:{"center_x":.85,"y":0.07}
+                    padding: [0, 125, 0, 0]
 
-        MDBottomNavigationItem:
-            name: 'screen 2'
-            text: 'Notizie'
-            icon: 'newspaper'
-            badge_icon: "numeric-5"
-            MDLabel:
-                text: 'Discord'
-                halign: 'center'
-        MDBottomNavigationItem:
-            name: 'screen 3'
-            text: 'Impostazioni'
-            icon: 'cog-outline'
-            MDLabel:
-                text: 'LinkedIN'
-                halign: 'center'
+                        
+                MDFloatingActionButton:
+                    icon: "magnify"
+                    type: "type_button"
+                    on_release: app.show_alert_dialog()
+                    pos_hint:{"center_x":.85,"y":0.07}
+
+            MDBottomNavigationItem:
+                name: 'screen 2'
+                text: 'Mappa'
+                icon: 'map'
+                badge_icon: "numeric-5"
+                MDLabel:
+                    text: 'Discord'
+                    halign: 'center'
+            MDBottomNavigationItem:
+                name: 'screen 3'
+                text: 'Impostazioni'
+                icon: 'cog-outline'
+                MDLabel:
+                    text: 'LinkedIN'
+                    halign: 'center'
+
+        MDToolbar
+            id: bar
+            title: "Orari GTT"
+            md_bg_color: 0, 0.5, 0.5, 1
+            elevation: 12
+            pos_hint: {"top":1}
+
+
+
 '''
 class Content(Screen):
     pass
 
+class MD3Card(MDCard, RoundedRectangularElevationBehavior):
+    '''Implements a material design v3 card.'''
+
+    text = StringProperty()
 
 class Test(MDApp):
     dialog = None
@@ -109,18 +144,36 @@ class Test(MDApp):
         self.root.ids.entries_box.clear_widgets()
         fermata =  self.dialog.content_cls.ids.name.text
 
-
+    
         response = requests.get('http://gpa.madbob.org/query.php?stop=' + fermata)
         json_data = response.json()
+        if response.text=="[]":
+            self.root.ids.entries_box.add_widget(
+                MD3Card(
+                    line_color=(0.2, 0.2, 0.2, 0.8),
+                    text="Linea inesistente",
+                    md_bg_color=get_color_from_hex("#f8f5f4"),
+                )
+            )
+            return
+        style="filled"
 
         for n in range(len(json_data)):
-            temp_button = Button(text="Linea " + json_data[n]['line'] + "                                           Ora: " + json_data[n]['hour'],
-                                 halign="left",
-                                 background_color=get_color_from_hex("#0000FF"),
-                                 border=(0,20,0,20))
-            self.root.ids.entries_box.add_widget(temp_button)
+            realtime=""
+            if json_data[n]['realtime']=="true":
+                realtime="*"
+            self.root.ids.entries_box.add_widget(
+                MD3Card(
+                    line_color=(0.2, 0.2, 0.2, 0.8),
+                    text="Linea " + json_data[n]['line'] + "                                           Ora: " + json_data[n]['hour']+ realtime,
+                    md_bg_color=get_color_from_hex("#f8f5f4"),
+                )
+            )
 
-
+    def on_start(self):
+        styles = {
+            "elevated": "#f6eeee", "filled": "#f4dedc", "outlined": "#f8f5f4"
+        }
 
     def build(self):
         self.theme_cls.material_style = "M3"
